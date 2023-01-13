7cd4a54486896f1d278d732a3878b5e571e61cb5
VeneraTolubaeva <venera.to@zohomail.com>
2023-01-13 22:07:53 +0300

    Add Java for Advance HomeWork1 and HomeWork2

---
+++ src/Lesson1.java
@@ -0,0 +1,14 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 10;
+        int b = 2;
+        System.out.println("a * b = " + (a * b));
+
+        char c = 'B';
+        System.out.println(c);
+
+        c = 'B' + 1;
+        System.out.println(c);
+    }
+}
